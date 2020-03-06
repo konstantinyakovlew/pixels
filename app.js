e5dbad9cf0bd130d8f85be8b673acf658765c272
konstantinyakovlew <konstantinyakovlew@gmail.com>
2020-03-06 20:12:58 +0300

    initial project setup

---
+++ app.js
@@ -0,0 +1,42 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+//const electron = require('electron');
+//const app = electron.app;
+const console = require('console');
+app.console = new console.Console(process.stdout, process.stderr);
+
+let window = null
+
+// Wait until the app is ready
+app.once('ready', () => {
+  // Create a new window
+  window = new BrowserWindow({
+    // Set the initial width to 500px
+    width: 500,
+    // Set the initial height to 400px
+    height: 400,
+    // set the title bar style
+    titleBarStyle: 'hiddenInset',
+    // set the background color to black
+    backgroundColor: "#111",
+    // Don't show the window until it's ready, this prevents any white flickering
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+  
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  window.once('ready-to-show', () => {
+    window.show()
+  })
+})
